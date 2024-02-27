ad2cee9756d942d4fa6c8158a280798a7006459a
mmk <mmkkarbalaei@gmail.com>
2024-02-27 23:05:24 +0330

    set sound

--- app.js
+++ app.js
@@ -24,5 +24,7 @@ btn.addEventListener('click', () => {
       <p class="word-example">
       ${data[0].meanings[0].definitions[0].example || ""}
       </p>`;
+      sound.setAttribute("src",`https:${data[0].phonetics[0].audio}`);
+
         });
 })
