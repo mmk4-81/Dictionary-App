9d1eaae022c22793348ba08e5c6b845d75dcea28
mmk <mmkkarbalaei@gmail.com>
2024-02-27 23:05:02 +0330

    Dynamic data placement

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const btn = document.getElementById('search-btn');
 btn.addEventListener('click', () => {
     let inpWord = document.getElementById('input-word').value;
     fetch(`${url}${inpWord}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+        .then((response) => response.json())
+        .then((data) => {
+            result.innerHTML =
+                `<div class="word">
+        <h3>${inpWord}</h3>
+        <button><i class="fas fa-volume-up"></i></button>
+      </div>
+      <div class="details">
+        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p>/${data[0].phonetic}/</p>
+      </div>
+      <p class="word-meaning">
+       ${data[0].meanings[0].definitions[0].definition}
+      </p>
+      <p class="word-example">
+      ${data[0].meanings[0].definitions[0].example || ""}
+      </p>`;
+        });
 })
