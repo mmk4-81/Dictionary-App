1e70193d38382772a1b17ae322ed5a9ea8ae40d6
mmk <mmkkarbalaei@gmail.com>
2024-02-27 23:12:08 +0330

    catch

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ btn.addEventListener('click', () => {
       sound.setAttribute("src",`https:${data[0].phonetics[0].audio}`);
 
         })
+        .catch(()=>{
+            result.innerHTML = `<h3>Couldn't Find The Word</h3>`;
+        })
        
 })
 
